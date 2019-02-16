211a207d85cd82738a4df704d30d48efa264316f
Thomas Mcfann <ttmcfann@go.olemiss.edu>
2019-02-16 17:33:23 -0600

    can add images

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validator';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -29,7 +30,8 @@ export class PostCreateComponent implements OnInit {
         validators: [Validators.required, Validators.minLength(3)]
       }),
       'content': new FormControl(null, {validators: [Validators.required]}),
-       'image': new FormControl(null, {validators: [Validators.required]})
+       'image': new FormControl(null, {validators: [Validators.required],
+        asyncValidators: [mimeType]})
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
@@ -40,7 +42,8 @@ export class PostCreateComponent implements OnInit {
           this.isLoading = false;
           this.post = {id: postData._id,
                        title: postData.title,
-                       content: postData.content
+                       content: postData.content,
+                       imagePath: null
           };
           this.form.setValue({
             'title': this.post.title,
@@ -72,7 +75,10 @@ export class PostCreateComponent implements OnInit {
     }
     this.isLoading = true;
     if (this.mode === 'create') {
-      this.postsService.addPost(this.form.value.title, this.form.value.content);
+      this.postsService.addPost(
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image);
     } else {
       this.postsService.updatePost(
         this.postId,
