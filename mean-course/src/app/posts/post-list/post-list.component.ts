d04891e1915228aec4e7ef63b86accaa721e5f40
Thomas Mcfann <ttmcfann@go.olemiss.edu>
2019-02-16 21:50:18 -0600

    added Backend for Pagination

--- mean-course/src/app/posts/post-list/post-list.component.ts
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -21,7 +21,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   isLoading = false;
   totalPosts = 10;
   postsPerPage = 2;
-  pageSizeOptions = [1,2, 5, 10];
+  pageSizeOptions = [1, 2, 5, 10];
   private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
