005936fa1cf7bd5b00dd190027643970b6974589
Thomas Mcfann <ttmcfann@go.olemiss.edu>
2019-03-10 18:53:31 -0500

    token is shown expiring

--- mean-course/src/app/auth/auth.service.ts
+++ mean-course/src/app/auth/auth.service.ts
@@ -9,6 +9,7 @@ export class AuthService {
   private isAuthenticated = false;
   private token: string;
   private authStatusListener = new Subject<boolean>();
+  private tokenTimer: any;
   constructor(private http: HttpClient, private router: Router) {}
 
   getToken() {
@@ -33,11 +34,15 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = {email: email, password: password};
-    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
+    this.http.post<{token: string, expiresIn: number}>('http://localhost:3000/api/user/login', authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
         if (token) {
+        const expiresInDuration = response.expiresIn;
+        this.tokenTimer = setTimeout(() => {
+          this.logout();
+        }, expiresInDuration * 1000);
         this.isAuthenticated = true;
         this.authStatusListener.next(true);
         this.router.navigate(['/']);
@@ -49,6 +54,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
     this.router.navigate(['/']);
 
   }
