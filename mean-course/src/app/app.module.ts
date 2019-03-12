0504166d0b29a174a6df0d70170c04495192093d
Thomas Mcfann <ttmcfann@go.olemiss.edu>
2019-03-12 13:33:57 -0500

    added an error dialogue

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -9,7 +9,8 @@ import { MatInputModule,
          MatToolbarModule,
          MatExpansionModule,
          MatProgressSpinnerModule,
-         MatPaginatorModule
+         MatPaginatorModule,
+         MatDialogModule
   } from '@angular/material';
 
 import { AppComponent } from './app.component';
@@ -20,6 +21,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
+import { ErrorInterceptor } from './error-interceptor';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +31,8 @@ import { AuthInterceptor } from './auth/auth-interceptor';
     PostListComponent,
     HeaderComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
@@ -43,9 +47,14 @@ import { AuthInterceptor } from './auth/auth-interceptor';
     MatExpansionModule,
     MatProgressSpinnerModule,
     HttpClientModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatDialogModule
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
-  bootstrap: [AppComponent]
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [ErrorComponent]
 })
 export class AppModule { }
---
+++ mean-course/src/app/error-interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { ErrorComponent } from './error/error.component';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private dialogue: MatDialog) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.dialogue.open(ErrorComponent);
+        return throwError(error);
+      })
+    );
+  }
+}
