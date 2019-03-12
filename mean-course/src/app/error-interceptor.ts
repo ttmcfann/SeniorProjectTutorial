4bf3b776dba989d610ca4f2f70c7ee8ff770eda9
Thomas Mcfann <ttmcfann@go.olemiss.edu>
2019-03-12 14:10:17 -0500

    dialogue box

--- mean-course/src/app/error-interceptor.ts
+++ mean-course/src/app/error-interceptor.ts
@@ -13,7 +13,11 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        this.dialogue.open(ErrorComponent);
+        let errorMessage = "An unkown error occurred!!";
+        if (error.error.message) {
+          errorMessage = error.error.message;
+        }
+        this.dialogue.open(ErrorComponent, {data: {message: errorMessage}});
         return throwError(error);
       })
     );
