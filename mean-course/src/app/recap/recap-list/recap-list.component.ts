f3a8d06b10d0d516c214e3696b4641bc9b1264ef
Thomas Mcfann <ttmcfann@go.olemiss.edu>
2019-03-18 15:32:14 -0500

    display recap

--- mean-course/src/app/recap/recap-list/recap-list.component.ts
+++ mean-course/src/app/recap/recap-list/recap-list.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recap-list',
-  templateUrl: './recap-list.component.html'
+  templateUrl: './recap-list.component.html',
+  styleUrls: ['./recap-list.component.css']
 })
 export class RecapListComponent {
+  // recaps = [
+  //   {title: 'First Recap', content: 'This is the first post\'s content'},
+  //   {title: 'Second Recap', content: 'This is the second post\'s content'},
+  //   {title: 'Third Recap', content: 'This is the third post\'s content'},
+  // ];
+  recaps = [];
 
 }
