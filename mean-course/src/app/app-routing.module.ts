0e2b8d1aa32fcbf63b47237dcd801f32c9fcec45
Thomas Mcfann <ttmcfann@go.olemiss.edu>
2019-03-20 21:03:16 -0500

    add other links

--- mean-course/src/app/app-routing.module.ts
+++ mean-course/src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { AuthGuard } from './auth/auth.guard';
+import { RecapListComponent } from './recap/recap-list/recap-list.component';
+import { RecapCreateComponent } from './recap/recap-create/recap-create.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
   { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard]},
+  { path: 'recap', component: RecapListComponent},
+  { path: 'createRecap', component: RecapCreateComponent},
   { path: 'auth', loadChildren: './auth/auth.module#AuthModule'}
+
+
 
 
 ];
