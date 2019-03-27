799e11e6d99891f4983e9efe1e322f59e0fdd0a4
Thomas Mcfann <ttmcfann@go.olemiss.edu>
2019-03-27 17:54:29 -0500

    added protection

--- mean-course/backend/routes/recaps.js
+++ mean-course/backend/routes/recaps.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Recap = require('../models/recap');
 const multer = require('multer');
 const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
 
 const MIME_TYPE_MAP = {
   'image/png': 'png',
@@ -25,7 +26,10 @@ const storage = multer.diskStorage({
   }
 });
 
-router.post("", multer({storage: storage}).single('image'),(req, res, next) => {
+router.post("",
+ checkAuth,
+ multer({storage: storage}).single('image'),
+ (req, res, next) => {
   const url = req.protocol + '://' + req.get('host');
   const recap = new Recap({
     title: req.body.title,
@@ -45,7 +49,8 @@ router.post("", multer({storage: storage}).single('image'),(req, res, next) => {
   });
 });
 
-router.put("/:id"
+router.put("/:id",
+checkAuth
 , multer({storage: storage}).single('image'),
 (req, res, next) => {
   let imagePath = req.body.imagePath;
@@ -104,7 +109,7 @@ router.get( "", (req,res,next) => {
 });
 
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
   Recap.deleteOne({_id: req.params.id}).then(result => {
     console.log(result);
     res.status(200).json({ message: "Post deleted!"});
