3a1caaaf9eb3f3bf8377fa6420ea6cb051d64992
Thomas Mcfann <ttmcfann@go.olemiss.edu>
2019-04-04 23:21:49 -0500

    fix bug

--- mean-course/backend/controllers/recaps.js
+++ mean-course/backend/controllers/recaps.js
@@ -44,7 +44,7 @@ exports.updateRecap = (req, res, next) => {
   });
   Recap.updateOne({ _id: req.params.id, creator: req.userData.userId }, recap)
     .then(result => {
-      if (result.nModified > 0) {
+      if (result.n > 0) {
         res.status(200).json({message: 'Update successful!'});
       } else {
         res.status(401).json({message: 'Not authorized!'});
