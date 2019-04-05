34fb2e452b7c4bd62c22a7823585701719f8df07
Thomas Mcfann <ttmcfann@go.olemiss.edu>
2019-04-04 23:03:38 -0500

    up to date

--- mean-course/src/app/angular-material.module.ts
+++ mean-course/src/app/angular-material.module.ts
@@ -11,16 +11,6 @@ import {
 } from '@angular/material';
 
 @NgModule({
-  imports: [
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDialogModule,
-  ],
   exports: [
     MatInputModule,
     MatCardModule,
